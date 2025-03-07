2bc55b62d9dcbb7eb3a11c7820e600fccd8172ad
rxdrigocrn <rodrigocirinocl@gmail.com>
2025-03-07 15:50:39 -0300

    Subindo o projeto inicial

---
+++ 2_OPERADORES/js/script.js
@@ -0,0 +1,33 @@
+// 1 - Number
+console.log(typeof 2)
+console.log(typeof 5.17)
+console.log(typeof -128)
+
+// 2 - operacoes aritmeticas
+console.log(2+4)
+console.log(10-5)
+console.log(5*4)
+console.log(10/2)
+
+console.log(5 + (4*2))
+
+// 3 - special numbers
+console.log(typeof Infinity)
+console.log(typeof -Infinity)
+
+console.log(12 * "afsaf")
+
+console.log(typeof NaN)
+
+// 4 - Strings
+console.log("Um texto")
+console.log(`Mais um texto`)
+console.log(typeof 'Outro texto')
+
+// 5 - Simbolos especiais em strings
+console.log("testando a \nquebra de linha")
+
+console.log("espacamento de \t tab")
+
+// 6 - Concatenação
+console.log("Um "+"texto "+"concatenado")
