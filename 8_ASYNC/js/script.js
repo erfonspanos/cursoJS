3517b5f124c444d812d0f1219e95a4835b46cb1d
erfonspanos <erfonspanos@gmail.com>
2025-04-03 16:26:45 -0300

    introduzindo Programação Assíncrona

---
+++ 8_ASYNC/js/script.js
@@ -0,0 +1,10 @@
+// 1 - setTimeout
+console.log("Ainda nao executou")
+
+setTimeout(function(){
+    console.log("Requisicao Assincrona")
+}, 2000)
+
+console.log("Ainda nao executou 2")
+
+// 2 - setInterval
