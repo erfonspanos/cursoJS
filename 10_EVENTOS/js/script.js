d2c273d4f1e1714a61d5c4d1a0ddea9d66ddbf3e
erfonspanos <erfonspanos@gmail.com>
2025-04-23 11:10:31 -0300

    Concluindo Eventos

--- 10_EVENTOS/js/script.js
+++ 10_EVENTOS/js/script.js
@@ -55,3 +55,81 @@ a.addEventListener("click", (e) => {
     e.preventDefault()
     console.log("nao alterou a pagina")
 })
+
+// 6 - Eventos de tecla
+document.addEventListener("keyup", (e) => {
+    console.log(`Soltou a tecla ${e.key}`)
+})
+
+document.addEventListener("keydown", (e) => {
+    console.log(`Pressionou a tecla ${e.key}`)
+})
+
+// 7 - Eventos de mouse
+const mouseEvents = document.querySelector("#mouse")
+
+mouseEvents.addEventListener("mousedown", () => {
+    console.log(`Pressionou o botao`)
+})
+
+mouseEvents.addEventListener("mouseup", () => {
+    console.log(`Soltou o botao`)
+})
+
+mouseEvents.addEventListener("dblclick", () => {
+    console.log(`Mouse deu clique duplo`)
+})
+
+// 8 - Movimento do mouse
+document.addEventListener("mousemove", (e) => {
+    // console.log(`No eixo x: ${e.x}`)
+    // console.log(`No eixo y: ${e.y}`)
+})
+
+// 9 - Evento de Scroll
+window.addEventListener("scroll", (e) => {
+    if(window.scrollY > 200){
+        console.log("Passamos de 200px")
+    }
+})
+
+// 10 - Evento de focus/blur
+const input = document.querySelector("#my-input")
+
+input.addEventListener("focus", (e) => {
+    console.log("Entrou no input")
+})
+
+input.addEventListener("blur", (e) => {
+    console.log("Saiu do input")
+})
+
+// 11 - Evento de carregamento
+window.addEventListener("load", () => {
+    console.log("a pagina carregou")
+})
+
+window.addEventListener("beforeunload", (e) => {
+    e.preventDefault();
+    e.returnValue = "";
+})
+
+// 12 - Debounce
+const debounce = (f, delay) => {
+
+    let timeout
+
+    return (...arguments) => {
+        if(timeout) {
+            clearTimeout(timeout)
+        }
+   
+        timeout = setTimeout(() => {
+            f.apply(arguments)
+        }, delay)
+    }
+}
+
+window.addEventListener("mousemove", debounce(() => {
+    console.log("executando a cada 400ms")
+}, 400))
